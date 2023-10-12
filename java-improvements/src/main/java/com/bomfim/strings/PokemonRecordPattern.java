cb042ac355b39f63d2cf321f8fceed4a18b63100
Thiago Bomfim <thiagobomfim1995@gmail.com>
2023-10-12 08:07:59 +0100

    Adding JEP 443: Unnamed Patterns and Variables (Preview), JEP 358: Helpful NullPointerExceptions and JEP 440: Record Patterns example

---
+++ java-improvements/src/main/java/com/bomfim/strings/PokemonRecordPattern.java
@@ -0,0 +1,80 @@
+package com.bomfim.strings;
+
+import com.bomfim.strings.RecordPokemon.PokemonRecord;
+
+import java.util.List;
+
+public class PokemonRecordPattern {
+
+    public record Pokeball(String name, PokemonRecord pokemon) {
+    }
+
+    public void printPokemon(Object obj) {
+        if (obj instanceof PokemonRecord p) {
+            int id = p.id();
+            String name = p.name();
+            System.out.println("name: " + name + "id: " + id);
+        }
+    }
+
+    public void printPokemon2(Object obj) {
+        if (obj instanceof PokemonRecord(Integer id, String name, List<String> type, String species)) {
+            System.out.println("name: " + name + "id: " + id);
+        }
+    }
+
+    public void printPokeball(Object obj) {
+        if (obj instanceof Pokeball(String name, PokemonRecord pokemon)) {
+            System.out.println("name: " + name);
+        }
+    }
+
+    /**
+     * Record Patterns - JEP 440: Record Patterns
+     * From Java 21
+     *
+     * @param obj pokeball
+     */
+    public void printPokeball2(Object obj) {
+        if (obj instanceof Pokeball(
+                String name, PokemonRecord(Integer id, String pokemonName, List<String> type, String species))) {
+            System.out.println("name: " + name + " - Pokemon: " + pokemonName);
+        }
+    }
+
+    public void printPokeball2Old(Object obj) {
+        if (obj instanceof Pokeball) {
+            var pokeball = ((Pokeball) obj);
+            var pokemon = pokeball.pokemon();
+            if (pokemon != null) {
+                System.out.println("name: " + pokeball.name + " - Pokemon: " + pokemon.name());
+            }
+        }
+    }
+
+    /**
+     * Helpful NullPointerExceptions - JEP 358: Helpful NullPointerExceptions
+     * From Java 14
+     *
+     * @param obj pokeball
+     */
+    public void printPokeball2OldNPE(Object obj) {
+        if (obj instanceof Pokeball) {
+            var pokeball = ((Pokeball) obj);
+            System.out.println("name: " + pokeball.name + " - Pokemon: " + pokeball.pokemon.name());
+        }
+    }
+
+    /**
+     * Unnamed Patterns and Variables - JEP 443: Unnamed Patterns and Variables (Preview)
+     * From Java 21
+     *
+     * @param obj pokeball
+     */
+    public void printPokeball3(Object obj) {
+        if (obj instanceof Pokeball(String name, PokemonRecord(var _, String pokemonName, var _, var _))) {
+            System.out.println("name: " + name + " - Pokemon: " + pokemonName);
+        }
+    }
+
+}
---
+++ java-improvements/src/test/java/com/bomfim/strings/PokemonRecordPatternTest.java
@@ -0,0 +1,98 @@
+package com.bomfim.strings;
+
+import com.bomfim.strings.PokemonRecordPattern.Pokeball;
+import com.bomfim.strings.RecordPokemon.PokemonRecord;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PokemonRecordPatternTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+    private PokemonRecordPattern pokemonRecordPattern;
+    private PokemonRecord pokemon;
+    private Pokeball pokeball;
+
+    @BeforeEach
+    void setUp() {
+        pokemonRecordPattern = new PokemonRecordPattern();
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+        pokemon = new PokemonRecord(1, "Bulbasaur", List.of("Grass", "Poison"), "Seed Pokémon");
+        pokeball = new Pokeball("Ultra Ball", pokemon);
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    void printPokemon() {
+        pokemonRecordPattern.printPokemon(pokemon);
+        assertThat(outContent.toString()).contains("Bulbasaur")
+                .contains("1");
+    }
+
+    @Test
+    void printPokemon2() {
+        pokemonRecordPattern.printPokemon2(pokemon);
+        assertThat(outContent.toString()).contains("Bulbasaur")
+                .contains("1");
+    }
+
+    @Test
+    void printPokeball() {
+        pokemonRecordPattern.printPokeball(pokeball);
+        assertThat(outContent.toString()).contains("Ultra Ball")
+                .doesNotContain("Bulbasaur");
+    }
+
+    @Test
+    void printPokeball2() {
+        pokemonRecordPattern.printPokeball2(pokeball);
+        assertThat(outContent.toString()).contains("Ultra Ball")
+                .contains("Bulbasaur");
+    }
+
+    @Test
+    void printPokeball2Old() {
+        pokemonRecordPattern.printPokeball2Old(pokeball);
+        assertThat(outContent.toString()).contains("Ultra Ball")
+                .contains("Bulbasaur");
+    }
+
+    @Test
+    void printPokeball2OldNPE() {
+        var pokeballEmpty = new Pokeball("Ultra Ball", null);
+        assertThatThrownBy(() -> pokemonRecordPattern.printPokeball2OldNPE(pokeballEmpty))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("Cannot invoke \"com.bomfim.strings.RecordPokemon$PokemonRecord.name()\" because \"pokeball.pokemon\" is null");
+    }
+
+    @Test
+    void printPokeball3() {
+        pokemonRecordPattern.printPokeball3(pokeball);
+        assertThat(outContent.toString()).contains("Ultra Ball")
+                .contains("Bulbasaur");
+    }
+
+    @Test
+    void printPokeball3WithoutPokemon() {
+        var pokeballEmpty = new Pokeball("Ultra Ball", null);
+        pokemonRecordPattern.printPokeball3(pokeballEmpty);
+        assertThat(outContent.toString()).isEmpty();
+    }
+}
